3c63a43965190918e4b5acdb353bc7041e5504ad
Ved Chitnis <ved.chitnis@gmail.com>
2021-10-03 21:15:40 +0530

    Changed ssl mode to require

--- config/db.go
+++ config/db.go
@@ -19,7 +19,7 @@ func InitMigration() {
 	dbname := os.Getenv("NAME")
 	pwd := os.Getenv("PASSWORD")
 
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%s TimeZone=Asia/Kolkata", host, user, dbname, pwd, dbport)
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=require password=%s port=%s TimeZone=Asia/Kolkata", host, user, dbname, pwd, dbport)
 	DB, err = gorm.Open(postgres.Open(dbUri), &gorm.Config{})
 	if err != nil {
 		panic("Cannot connect to DB")
