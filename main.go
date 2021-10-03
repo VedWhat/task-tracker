d5c09370b3ccd8218f41493bfb9b1dafd9e6585d
Ved Chitnis <ved.chitnis@gmail.com>
2021-10-03 21:05:47 +0530

    Changed port for deployment

--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
 	"task-tracker/src/config"
 	"task-tracker/src/models"
 	"task-tracker/src/routers"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT not set defaulting to 9999")
+		port = "9999"
+	}
 	config.InitMigration()
 	config.DB.AutoMigrate(&models.User{})
 	config.DB.AutoMigrate(&models.Task{})
 
 	router := routers.InitRouter()
-	router.Run(":9999")
+	router.Run(":" + port)
 }
